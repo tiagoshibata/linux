2effc712d60865f8061c9216bfa8c78d0336b5cc
Carlos Eduardo Garcia <carlos.sena.garcia@usp.br>
2019-05-31 14:34:44 -0300

    adiciona pasta

---
+++ sys_write_read_number/sys_write_read_number.c
@@ -0,0 +1,15 @@
+#include<linux/linkage.h>
+#include<linux/list.h>
+#include<linux/module.h>
+#include<linux/slab.h>
+
+static int x;
+
+asmlinkage long sys_write_number(int number){
+	x = number;
+	return 0;
+}
+
+asmlinkage long sys_read_number(void){
+	return x;
+}
